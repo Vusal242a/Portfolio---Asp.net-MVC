ea2078128c27530e67b40ecf5c7e23208be54d25
Vusal I. Mahmudlu <VusalIm@CODE.LOCAL>
2019-05-22 21:27:42 +0400

    final aspa

---
+++ ASP.net Final/ASPNetFinalSolution/ASPNetFinal/Models/CvDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ASPNetFinal.Models
+{
+    public class CvDbContext: DbContext
+    {
+        public CvDbContext()
+            :base ("name=cString")
+        {
+
+        }
+        public DbSet<Person> Person { get; set; }
+    }
+}
