46e15c1966dd62bf3a2d33792fb1897b4e65070c
Vusal I. Mahmudlu <VusalIm@CODE.LOCAL>
2019-05-22 21:09:34 +0400

    final asp

---
+++ ASP.net Final/ASPNetFinalSolution/ASPNetFinal/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPNetFinal.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your about me page.";
+            return View();
+        }
+
+        public ActionResult Resume()
+        {
+            ViewBag.Message = "Your resume page.";
+            return View();
+        }
+
+        public ActionResult Portfolio()
+        {
+            ViewBag.Message = "Your portfolio page.";
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            ViewBag.Message = "Your blog page.";
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+        public ActionResult Sidebar()
+        {
+            ViewBag.Message = "Your sidebar.";
+            return View();
+        }
+    }
+}
