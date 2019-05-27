a77913e5b49ac614831b17a2aa9de9aec56729a5
Vusal I. Mahmudlu <VusalIm@CODE.LOCAL>
2019-05-27 20:29:11 +0400

    session autorization + Vizew

---
+++ ASP.net Final/Vizew.WebUI/Models/Entity/News.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Vizew.WebUI.Models.Entity
+{
+    public class News
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string MediaUrl { get; set; }
+        [StringLength(70)]
+        public string Author { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        [VizewSqlDefaultValue(DefaultValueSql = "1")]
+        public bool IsPopular { get; set; }
+
+        [VizewDatabaseGenerated(DatabaseGeneratedOption.Computed, DefaultValueSql = "getdate()")]
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ ASP.net Final/Vizew.WebUI/Models/VizewDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using Vizew.WebUI.Migrations;
+using Vizew.WebUI.Models.Entity;
+
+namespace Vizew.WebUI.Models
+{
+    public class VizewDbContext : DbContext
+    {
+        public VizewDbContext()
+            :base("name=cString")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<VizewDbContext,Configuration>());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Add(new AttributeToColumnAnnotationConvention<VizewDatabaseGeneratedAttribute, string>("VizewDatabaseGenerated", (p, attributes) => attributes.Single().DefaultValueSql));
+            modelBuilder.Conventions.Add(new AttributeToColumnAnnotationConvention<VizewSqlDefaultValueAttribute, string>("VizewSqlDefaultValue", (p, attributes) => attributes.Single().DefaultValueSql));
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<News> News { get; set; }
+    }
+}
